ede8543d83d88af4bcccae5ba8047f3706a4716e
Sara Bornstein <sbornste@gmail.com>
2020-04-01 17:01:20 -0800

    conditions_loops Excercise 11 Done

--- src/labs_examples/conditions_loops/labs/Exercise_11.java
+++ src/labs_examples/conditions_loops/labs/Exercise_11.java
@@ -21,6 +21,20 @@ package labs_examples.conditions_loops.labs;
  *
  */
 
-public class Exercise_11 {
+public class Exercise_11
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i<=10; i++)
+        {
+            System.out.print(i + " ");
+
+            for (int j = 2; j <= 10; j++)
+            {
+                System.out.print("|" + i * j);
+            }
+            System.out.println("|");
+        }
+    }
 
 }
