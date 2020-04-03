be2e5dc29df104e49c2864e29988219a7e135e9f
Sara Bornstein <sbornste@gmail.com>
2020-04-03 13:42:43 -0800

    Arrays labs Exercises 5 & 7 done

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,40 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+        ArrayList<String> arrList = new ArrayList();
+        arrList.add("head");
+        arrList.add("shoulders");
+        arrList.add("knees");
+        arrList.add("toes");
+        arrList.add("eyes");
+        arrList.add("ears");
+        arrList.add("nose");
+        arrList.add("mouth");
+
+        System.out.println(arrList);
+
+        int size = arrList.size();
+        System.out.println("The arrayList has " + size + " items.");
+
+        if(arrList.contains("knees")){
+            System.out.println(true);
+        } else{
+            System.out.println(false);
+        }
+
+        arrList.remove(arrList.get(2));
+        System.out.println(arrList);
+
+        if(arrList.contains("knees")){
+            System.out.println(true);
+        } else{
+            System.out.println(false);
+        }
+
+        arrList.clear();
+        System.out.println(arrList);
+
+
+    }
 }
