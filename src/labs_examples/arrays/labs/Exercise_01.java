7609e1290a96daebf2f4e0aeb5377250ff476ecc
Sara Bornstein <sbornste@gmail.com>
2020-04-02 18:22:46 -0800

    Arrays Exercise 1 & 2 done

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,4 +1,5 @@
 package labs_examples.arrays.labs;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -11,5 +12,51 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number 1: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter number 2: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter number 3: ");
+        int num3 = scanner.nextInt();
+
+        System.out.print("Enter number 4: ");
+        int num4 = scanner.nextInt();
+
+        System.out.print("Enter number 5: ");
+        int num5 = scanner.nextInt();
+
+        System.out.print("Enter number 6: ");
+        int num6 = scanner.nextInt();
+
+        System.out.print("Enter number 7: ");
+        int num7 = scanner.nextInt();
+
+        System.out.print("Enter number 8: ");
+        int num8 = scanner.nextInt();
+
+        System.out.print("Enter number 9: ");
+        int num9 = scanner.nextInt();
+
+        System.out.print("Enter number 10: ");
+        int num10 = scanner.nextInt();
+
+        int[] arr = {num1, num2, num3, num4, num5, num6, num7, num8, num9, num10};
+
+        int sum = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+
+        System.out.println("The sum = " + sum);
+
+        int avg = sum / arr.length;
+        System.out.println("The average = " + avg);
+    }
 }
