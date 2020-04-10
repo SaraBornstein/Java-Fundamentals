ce522bcede60fc4bbfd5b3437b4ad885c31dc2c4
Sara Bornstein <sbornste@gmail.com>
2020-04-09 17:21:29 -0800

    Arrays lab Exercise 3 DONE

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -22,26 +22,23 @@ public class Exercise_03 {
         int[][] twoDArray = new int[5][5];
         // use 2 for loops to populate the 2d array
         // first for loop
+        for (int i = 0; i < twoDArray.length; i++){
+            for (int j = 0; j < twoDArray[i].length; j++){
+                twoDArray[j][i] = 3 + j*3 + 15*i;
 
-        for (int i = 0; i < twoDArray.length; i++)
-        {
-            for (int j = 0; j < twoDArray[i].length; j++)
-            {
-                twoDArray[j][i] = i*j;
-
+                // 1*3 2*3 3*3 4*3 5*3
+                // 6*3 7*3 8*3 9*3 10*3
+                // 11*3 12 13 14 15
+                // 16 17 18 19 20
+                // 21 22 23 24 25
             }
-
         }
 
-        for (int i = 0; i < twoDArray.length; i++)
-        {
-            for (int j = 0; j < twoDArray[i].length; j++)
-            {
+        for (int i = 0; i < twoDArray.length; i++){
+            for (int j = 0; j < twoDArray[i].length; j++){
                 System.out.print(twoDArray[j][i] + " ");
             }
             System.out.println();
         }
-
-
     }
 }
