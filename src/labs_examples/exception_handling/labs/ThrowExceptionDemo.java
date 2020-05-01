57ac96c62082cf61240b81decd23abe50ca1d6e3
Sara Bornstein <sbornste@gmail.com>
2020-05-01 11:45:44 -0800

    Labs exception_handling Exercise_05

---
+++ src/labs_examples/exception_handling/labs/ThrowExceptionDemo.java
@@ -0,0 +1,23 @@
+package labs_examples.exception_handling.labs;
+
+/**
+ * Exception Handling Exercise 5:
+ *
+ *      Demonstrate how to throw an exception.
+ *
+ */
+
+public class ThrowExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            divide();
+        }catch(ArithmeticException exc){
+            System.out.println("This won't work because I said so.");
+        }
+    }
+    
+    public static int divide() throws ArithmeticException{
+            int x = 100 / 0;
+            return x;
+    }
+}
