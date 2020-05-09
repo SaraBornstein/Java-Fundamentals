c727af9862840e67e593b9f8548de6ae4ae1f89d
Sara Bornstein <sbornste@gmail.com>
2020-05-08 16:08:31 -0800

    Input_Output labs Exercise_01

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +14,39 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+        BufferedInputStream bin = null;
 
+        String filePath = "src/labs_examples/input_output/files/char_data.txt";
+        String fileWritePath = "src/labs_examples/input_output/labs/char_data_rewrite.txt";
 
+        try {
+            bin = new BufferedInputStream(new FileInputStream(filePath));
+            fout = new FileOutputStream(fileWritePath);
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
+
+            while ((bytesRead = bin.read(buffer)) != -1) {
+                fout.write(buffer, 0, bytesRead);
+            }
+        } catch (FileNotFoundException exc) {
+            exc.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                //fin.close();
+                if(fin != null) {
+                    fin.close();
+                }
+                bin.close();
+                if(fout != null) {
+                    fout.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
