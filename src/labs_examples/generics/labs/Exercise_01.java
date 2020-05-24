0d652c824e7b7fd30a56b059fc32f99949fbe8bf
Sara Bornstein <sbornste@gmail.com>
2020-05-23 18:30:32 -0800

    generics Exercise01

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,48 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+class GenericClassExample <T>{
+    public static void main(String[] args) {
+        GenericClassExample <String> obj1 = new GenericClassExample("one", "two");
+        GenericClassExample <Integer> obj2 = new GenericClassExample(1,2);
+        GenericClassExample <Double> obj3 = new GenericClassExample(1.2,2.3);
+
+        obj1.print();
+        obj2.print();
+        obj3.print();
+
+
+    }
+
+    //2 instance variables
+    private T val1;
+    private T val2;
+
+    public void print(){
+        System.out.println(val1 + " and " + val2);
+    }
+
+    //constructor
+    public GenericClassExample(T val1, T val2) {
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+
+//getters & setters
+
+    public T getVal1() {
+        return val1;
+    }
+
+    public void setVal1(T val1) {
+        this.val1 = val1;
+    }
+
+    public T getVal2() {
+        return val2;
+    }
+
+    public void setVal2(T val2) {
+        this.val2 = val2;
+    }
+}
