c67b4b88845e219106d7b2203268e47eb102cda8
Sara Bornstein <sbornste@gmail.com>
2020-05-24 17:16:53 -0800

    working on generics labs

--- src/labs_examples/generics/labs/Exercise_02.java
+++ src/labs_examples/generics/labs/Exercise_02.java
@@ -1,8 +1,22 @@
 package labs_examples.generics.labs;
 
+import java.util.ArrayList;
+
 /**
  * Generics Exercise 2:
  *
  *      Create a class with a generic method that takes in an ArrayList of any Numeric type and returns the sum of all
  *      Numbers in the ArrayList
  */
+
+class GenericExerciseTwo {
+
+    public static <T extends Number> Number T (ArrayList<? extends Number> arr){
+        Number sum = 0;
+        for(Number elem : arr){
+            //sum = sum + elem;
+        }
+        return sum;
+
+    }
+}
--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -17,3 +17,46 @@ import java.util.Collection;
  *      4) Write a generic method to find the largest element within the range (begin, end) of a list.
  *
  */
+
+
+class GenericExerciseThree {
+    public static void main(String[] args) {
+        System.out.println(addition(3.4, 5f));
+
+        ArrayList<String> strArr = new ArrayList();
+        strArr.add("Hello");
+        strArr.add("Goodbye");
+        strArr.add("toot");
+        palindromeCount(strArr); //TODO not printing
+    }
+    //1
+    public static <T extends Number> Double addition(T a, T b){
+        double sum = a.doubleValue() + b.doubleValue();
+        return sum;
+    }
+    //2
+    public static <E> int palindromeCount(ArrayList<String> palindromes){
+        int count = 0;
+        for(String str : palindromes){
+
+            StringBuilder input1 = new StringBuilder();
+
+            input1.append(str); // append a string into StringBuilder input1
+
+            input1 = input1.reverse(); // reverse StringBuilder input1
+
+            if(str.equalsIgnoreCase(input1.toString()))
+                count++;
+        }
+        return count;
+    }
+
+    //3
+    public static <P> void changePositions(P[] parr){
+        P temp = parr[0];
+
+        for(int i = 0; i < parr.length; i++){
+
+        }
+    }
+}
