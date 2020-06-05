04e135eb63c42846a1001dda129a010e1224e9ca
Sara Bornstein <sbornste@gmail.com>
2020-06-04 17:30:31 -0800

    Lambdas Exercise_03 1 Done

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,21 +1,31 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.Function;
+
 /**
  * Lambdas Exercise 3:
- *
  *      1) Demonstrate the use of a static method reference
  *      2) Demonstrate the use of an instance method reference
  *      3) Demonstrate the use of a constructor reference
- *
  */
+class ExerciseThree{
+    public static void main(String[] args) {
+// 1) Demonstrate the use of a static method reference (Class::staticMethod)
+        Function<Integer,Integer> obj1 = DoSomething :: printAnInt;
+        int x = obj1.apply(5);
+        System.out.println(x);
 
-//1. Function
-//2. Supplier
-//3. Consumer
-//4. DoublePredicate
-//5. DoubleToIntFunction
-//6. LongBinaryOperator
-//7. ToIntBiFunction
-//8. BooleanSupplier
-//9. BiPredicate
-//10. IntFunction
+// 2) Demonstrate the use of an instance method reference (ObjectType::instanceMethod)
+
+
+    }
+}
+
+class DoSomething{
+    public static Integer printAnInt (int t) {
+        return t + 2;
+    }
+}
+
+
+
